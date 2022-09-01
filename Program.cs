bebdfaca78371be5ef91d40da897c13d0d1d66e9
CAPMAT CAPMATOB <lipa5511@mail.ru>
2022-09-01 14:16:50 +0400

    Внесены изменения

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double[] A = new double [3];
+double res;
+for (int i = 0; i < 3;) {
+    Console.WriteLine($"Введите {i+1} координату точки А");
+A[i] = Convert.ToDouble(Console.ReadLine());
+i += 1;
+}
+double[] B = new double [3];
+for (int i = 0; i < 3;) {
+    Console.WriteLine($"Введите {i+1} координату точки B");
+B[i] = Convert.ToDouble(Console.ReadLine());
+i += 1;
+}
+res = Math.Sqrt(Math.Pow(B[0] - A[0], 2) + Math.Pow(B[1] - A[1], 2) + Math.Pow(B[2] - A[2], 2));
+Console.WriteLine($"Расстояние между двумя точками = {res}");
